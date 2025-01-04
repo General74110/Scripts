c85e907de5406f3d3df0ab12e5463b9a8fbe3c97
General74110 <6901777@gmail.com>
2025-01-04 19:47:19 +0800

    git push origin master

--- Quantumult X/Script/Task/follow_cookie.js
+++ Quantumult X/Script/Task/follow_cookie.js
@@ -7,7 +7,7 @@ token=xxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxx"从authjs.session-
 token=xxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxx开始的内容
 
 [Script]
-http-request ^https?:\/\/app\.follow\.is\?/ script-path=https://raw.githubusercontent.com/General74110/Config/refs/heads/master/Script/Task/follow_cookie.js, requires-body=true, timeout=60, enabled=false, tag=酷我音乐获取Cookies, img-url=https://raw.githubusercontent.com/deezertidal/private/main/icons/kuwosvip.png
+http-request ^https?:\/\/app\.follow\.is\?/ script-path=https://raw.githubusercontent.com/General74110/Scripts/refs/heads/master/Quantumult%20X/Script/Task/follow_cookie.js, requires-body=true, timeout=60, enabled=false, tag=酷我音乐获取Cookies, img-url=https://raw.githubusercontent.com/deezertidal/private/main/icons/kuwosvip.png
 [MITM]
 hostname = app.follow.is
 
