a26c89e615955921981ecd3349bfed90f7a9641a
General74110 <6901777@gmail.com>
2025-10-06 18:48:17 +0800

    Update

--- Script/Adblock/SqxsAd.js
+++ Script/Adblock/SqxsAd.js
@@ -3,15 +3,7 @@ let obj = JSON.parse($response.body);
 // 获取请求地址
 let requestUrl = $request.url;
 // 判断是否为匹配项
-if (/^https:\/\/(ssp-sdk-config\.uc\.cn\/mediations|ocean\.shuqireader\.com\/api\/ad\/adserver\/v1\/api\/getAdInfo\??)/.test(requestUrl)) {
-    //判断是否存在数据
-
-    if (obj.hasOwnProperty("data")) {
-        delete obj.data;
-
-    }
-
-    } else if (/^https:\/\/ocean\.shuqireader\.com\/sqios\/render\/render\/page\/bookstore/.test(requestUrl)) {
+ if (/^https:\/\/ocean\.shuqireader\.com\/sqios\/render\/render\/page\/bookstore/.test(requestUrl)) {
 
     if (json.data && json.data.moduleInfos && Array.isArray(json.data.moduleInfos) && json.data.moduleInfos.length > 1) {
         delete json.data.moduleInfos[1];// 章末推荐书籍
@@ -19,9 +11,12 @@ if (/^https:\/\/(ssp-sdk-config\.uc\.cn\/mediations|ocean\.shuqireader\.com\/api
     if (json.data && json.data.props && json.data.props["x-preProcessor"] && Array.isArray(json.data.props["x-preProcessor"])) {
         delete json.data.props["x-preProcessor"][0]; // 书城横幅
     }
+} else if (/https:\/\/ocean\.shuqireader\.com\/sqios\/render\/render\/native\/page\/scene/.test(requestUrl)) {
+    if (obj.hasOwnProperty("data")) {
+        delete obj.data;
+    }
+
 }
-
-
 
 
 //重写数据
