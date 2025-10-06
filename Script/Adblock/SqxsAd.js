75970a9671f254603aa7789ab7a6e747f9ccb230
General74110 <6901777@gmail.com>
2025-10-06 18:13:14 +0800

    Update

--- Script/Adblock/SqxsAd.js
+++ Script/Adblock/SqxsAd.js
@@ -9,10 +9,18 @@ if (/^https:\/\/(ssp-sdk-config\.uc\.cn\/mediations|ocean\.shuqireader\.com\/api
     if (obj.hasOwnProperty("data")) {
         delete obj.data;
 
-        console.log(obj);
+    }
 
+    } else if (/^https:\/\/ocean\.shuqireader\.com\/sqios\/render\/render\/page\/bookstore$/.test(requestUrl)) {
+
+    if (json.data && json.data.moduleInfos && Array.isArray(json.data.moduleInfos) && json.data.moduleInfos.length > 1) {
+        delete json.data.moduleInfos[1];// 章末推荐书籍
+    }
+    if (json.data && json.data.props && json.data.props["x-preProcessor"] && Array.isArray(json.data.props["x-preProcessor"])) {
+        delete json.data.props["x-preProcessor"][0]; // 书城横幅
     }
 }
+
 
 
 
