4eabe0badc150721b302cdbe65f6c56f1042cb90
General74110 <6901777@gmail.com>
2025-10-06 16:59:51 +0800

    Update

--- Script/Adblock/SqxsAd.js
+++ Script/Adblock/SqxsAd.js
@@ -3,7 +3,7 @@ let obj = JSON.parse($response.body);
 // 获取请求地址
 let requestUrl = $request.url;
 // 判断是否为匹配项
-if (/^https:\/\/ocean\.shuqireader\.com\/api\/ad\/adserver\/v1\/api\/getAdInfo?\?/.test(requestUrl)) {
+if (/^https:\/\/(ssp-sdk-config\.uc\.cn\/mediations|ocean\.shuqireader\.com\/api\/ad\/adserver\/v1\/api\/getAdInfo\??)/.test(requestUrl)) {
     //判断是否存在数据
 
     if (obj.hasOwnProperty("data")) {
