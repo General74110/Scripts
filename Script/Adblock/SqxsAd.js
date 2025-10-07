43336740e15992decfb31076944d7d4fcb440418
General74110 <6901777@gmail.com>
2025-10-07 22:48:30 +0800

    Update

--- Script/Adblock/SqxsAd.js
+++ Script/Adblock/SqxsAd.js
@@ -5,36 +5,36 @@ let requestUrl = $request.url;
 // 判断是否为匹配项
  if (/^https:\/\/ocean\.shuqireader\.com\/sqios\/render\/render\/page\/bookstore/.test(requestUrl)) {
 
-    if (json.data && json.data.moduleInfos && Array.isArray(json.data.moduleInfos) && json.data.moduleInfos.length > 1) {
-        delete json.data.moduleInfos[1];// 章末推荐书籍
+    if (obj.data && obj.data.moduleInfos && Array.isArray(obj.data.moduleInfos) && obj.data.moduleInfos.length > 1) {
+        delete obj.data.moduleInfos[1];// 章末推荐书籍
     }
-    if (json.data && json.data.props && json.data.props["x-preProcessor"] && Array.isArray(json.data.props["x-preProcessor"])) {
-        delete json.data.props["x-preProcessor"][0]; // 书城横幅
+    if (obj.data && obj.data.props && obj.data.props["x-preProcessor"] && Array.isArray(obj.data.props["x-preProcessor"])) {
+        delete obj.data.props["x-preProcessor"][0]; // 书城横幅
     }
 } else if (/^https:\/\/ocean\.shuqireader\.com\/sqios\/render\/render\/native\/page\/scene/.test(requestUrl)) {
-    if (obj.hasOwnProperty("data")) {
-        delete obj.data;
+    if (obj.data) {
+        obj.data = {};
     }
 
 } else if (/^https:\/\/ocean\.shuqireader\.com\/api\/route\/iosReadPage\/adV2\?/.test(requestUrl)) {
-     if (obj.hasOwnProperty("data")) {
-         delete obj.data;
+     if (obj.data) {
+         obj.data = {};
      }
  } else if (/^https:\/\/ocean\.shuqireader\.com\/api\/route\/iosReadPage\/adTurnChapter\?/.test(requestUrl)) {
-     if (obj.hasOwnProperty("data")) {
-         delete obj.data;
+     if (obj.data) {
+         obj.data = {};
      }
  } else if (/^https:\/\/ocean\.shuqireader\.com\/api\/route\/ios\/readPage/.test(requestUrl)) {
-     if (obj.hasOwnProperty("data")) {
-         delete obj.data;
+     if (obj.data) {
+         obj.data = {};
      }
  } else if (/^https:\/\/partner\.uc\.cn\/realtime_config/.test(requestUrl)) {
-     if (obj.hasOwnProperty("data")) {
-         delete obj.data;
+     if (obj.data) {
+         obj.data = {};
      }
  } else if (/^https:\/\/huichuan\.sm\.cn\/nativead/.test(requestUrl)) {
-     if (obj.hasOwnProperty("data")) {
-         delete obj.data;
+     if (obj.data) {
+         obj.data = {};
      }
  }
 
